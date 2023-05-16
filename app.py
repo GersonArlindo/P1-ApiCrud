71d15e37621683d2bb985fd68593086073c0c28e
GersonArlindo <gerson50039@gmail.com>
2023-05-15 22:01:06 -0600

    Ruta para traer posts

--- app.py
+++ app.py
@@ -2,6 +2,12 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-app.get('/')
+posts = []
+
+@app.get('/')
 def read_root():
     return{"Welcome": "wolcome to my REST API"}
+
+@app.get('/posts')
+def get_posts():
+    return posts
